0e4d69275e57cd0b3bd2767c03f6336335501803
Djenya <kimjohn1@gmail.com>
2016-11-28 20:57:24 +0600

    tableView

--- Services/Services/ServicesTableViewController.h
+++ Services/Services/ServicesTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ServicesTableViewController.h
 //  Services
 //
 //  Created by Istore on 28.11.16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ServicesTableViewController : UITableViewController
 
 @end
-
